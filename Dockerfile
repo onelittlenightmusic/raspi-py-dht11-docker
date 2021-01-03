1181559c3a79a65b68e02fef5c15c22dde32b7cb
Hiro Osaki <hiroyuki.osaki@gmail.com>
2021-01-03 10:08:01 -0800

    grafana / prometheus addition

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,7 @@ FROM balenalib/raspberrypi3-debian-python:latest
 
 WORKDIR /usr/src/app
 RUN sudo apt update && sudo apt install -y gcc libc6-dev
-RUN pip install dht11
+RUN pip install dht11 flask
 COPY app/ /usr/src/app/
-CMD ["python", "./app.py"]
+CMD ["python", "./app-exporter.py"]
+#CMD ["python", "-u", "./app.py"]
---
+++ app/app-exporter.py
@@ -0,0 +1,26 @@
+from flask import Flask
+import dht11
+import RPi.GPIO as GPIO
+
+GPIO.setwarnings(True)
+GPIO.setmode(GPIO.BCM)
+
+pin = int(os.environ.get('DHT11_PIN_NUMBER'))
+interval = int(os.environ.get('DHT11_INTERVAL'))
+prefix = os.environ.get('DHT11_PREFIX')
+instance = dht11.DHT11(pin)
+
+app = Flask(__name__)
+
+@app.route("/metrics")
+def metrics():
+    dht11_data = ""
+    result = instance.read()
+    if result.is_valid():
+        dht11_data = f"""{prefix}_temperature {result.temperature}
+{prefix}_humidity {result.humidity}"""
+
+    return f"{dht11_data}", 200, {'Content-Type': 'text/plain; charset=utf-8'}
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
