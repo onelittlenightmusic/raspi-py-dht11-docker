1697c57814d2ede77285b30cd892793013045bac
Hiro Osaki <hiroyuki.osaki@gmail.com>
2021-01-03 13:12:46 -0800

    fix prometheus tag

--- app/app-exporter.py
+++ app/app-exporter.py
@@ -8,7 +8,7 @@ GPIO.setmode(GPIO.BCM)
 
 pin = int(os.environ.get('DHT11_PIN_NUMBER'))
 interval = int(os.environ.get('DHT11_INTERVAL'))
-prefix = os.environ.get('DHT11_PREFIX')
+tag = os.environ.get('DHT11_TAG')
 instance = dht11.DHT11(pin)
 
 app = Flask(__name__)
@@ -18,8 +18,8 @@ def metrics():
     dht11_data = ""
     result = instance.read()
     if result.is_valid():
-        dht11_data = f"""{prefix}_temperature {result.temperature}
-{prefix}_humidity {result.humidity}"""
+        dht11_data = f"""temperature{{{tag}}} {result.temperature}
+humidity{{{tag}}}} {result.humidity}"""
 
     return f"{dht11_data}", 200, {'Content-Type': 'text/plain; charset=utf-8'}
 
